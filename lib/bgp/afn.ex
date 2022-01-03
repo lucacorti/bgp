e69d8784d81d075c39321f17b26e65bd0aece64d
Luca Corti <luca.corti@dottori.it>
2022-01-03 11:15:37 +0100

    Harmonize and typespecs

--- lib/bgp/afn.ex
+++ lib/bgp/afn.ex
@@ -5,20 +5,24 @@ defmodule BGP.AFN do
   @type afi :: :reserved | :ipv4 | :ipv6
   @type safi :: :reserved | :nlri_unicast | :nlri_multicast
 
+  @spec decode_afi(code()) :: afi()
   def decode_afi(0), do: :reserved
   def decode_afi(1), do: :ipv4
   def decode_afi(2), do: :ipv6
   def decode_afi(65_535), do: :reserved
 
+  @spec decode_safi(code()) :: safi()
   def decode_safi(0), do: :reserved
   def decode_safi(1), do: :nlri_unicast
   def decode_safi(2), do: :nlri_multicast
   def decode_safi(255), do: :reserved
 
+  @spec encode_afi(afi()) :: code()
+  def encode_afi(:reserved), do: 0
   def encode_afi(:ipv4), do: 1
   def encode_afi(:ipv6), do: 2
-  def encode_afi(:reserved), do: 65_535
 
+  @spec encode_safi(safi()) :: code()
   def encode_safi(:reserved), do: 0
   def encode_safi(:nlri_unicast), do: 1
   def encode_safi(:nlri_multicast), do: 2
