61393e93df82fc4f88a4d35be1851d742a67602e
Luca Corti <luca.corti@dottori.it>
2022-01-05 18:20:04 +0100

    Rename subtype normal to route_refresh

--- lib/bgp/message/route_refresh.ex
+++ lib/bgp/message/route_refresh.ex
@@ -3,10 +3,10 @@ defmodule BGP.Message.ROUTEREFRESH do
 
   alias BGP.AFN
 
-  @type subtype :: :normal | :borr | :eorr
+  @type subtype :: :route_refresh | :borr | :eorr
   @type t :: %__MODULE__{afi: AFN.afi(), safi: AFN.safi()}
   @enforce_keys [:afi, :safi]
-  defstruct afi: nil, safi: nil, subtype: :normal
+  defstruct afi: nil, safi: nil, subtype: :route_refresh
 
   alias BGP.Message.Encoder
 
@@ -24,7 +24,7 @@ defmodule BGP.Message.ROUTEREFRESH do
     end
   end
 
-  defp decode_subtype(0), do: {:ok, :normal}
+  defp decode_subtype(0), do: {:ok, :route_refresh}
   defp decode_subtype(1), do: {:ok, :borr}
   defp decode_subtype(2), do: {:ok, :eorr}
   defp decode_subtype(_code), do: {:error, %Encoder.Error{code: :route_refresh_message}}
@@ -33,7 +33,7 @@ defmodule BGP.Message.ROUTEREFRESH do
   def encode(%__MODULE__{afi: afi, safi: safi, subtype: subtype}, _options),
     do: [<<AFN.encode_afi(afi)::16, encode_subtype(subtype)::8, AFN.encode_safi(safi)::8>>]
 
-  defp encode_subtype(:normal), do: 0
+  defp encode_subtype(:route_refresh), do: 0
   defp encode_subtype(:borr), do: 1
   defp encode_subtype(:eorr), do: 2
 end
