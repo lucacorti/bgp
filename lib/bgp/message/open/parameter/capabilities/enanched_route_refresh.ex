0a2690b3ca3e51371037a8516a15447680690c94
Luca Corti <luca.corti@dottori.it>
2022-01-05 18:20:38 +0100

    Add missing file

---
+++ lib/bgp/message/open/parameter/capabilities/enanched_route_refresh.ex
@@ -0,0 +1,16 @@
+defmodule BGP.Message.OPEN.Parameter.Capabilities.EnanchedRouteRefresh do
+  @moduledoc false
+
+  @type t :: %__MODULE__{}
+  defstruct []
+
+  alias BGP.Message.Encoder
+
+  @behaviour Encoder
+
+  @impl Encoder
+  def decode(<<>>, _options), do: {:ok, %__MODULE__{}}
+
+  @impl Encoder
+  def encode(_multi_protocol, _options), do: []
+end
