476a68c4ea573b067596320d1b5cc29093bf0197
Luca Corti <luca@fantacast.it>
2023-06-25 20:16:57 +0200

    Use start_supervised! because start_link_supervised! is 1.14+ only

--- test/bgp/server_test.exs
+++ test/bgp/server_test.exs
@@ -3,8 +3,8 @@ defmodule BGP.ServerTest do
 
   setup_all _ctx do
     %{
-      server_a: start_link_supervised!({BGP.Server, BGP.TestServerA}),
-      server_b: start_link_supervised!({BGP.Server, BGP.TestServerB})
+      server_a: start_supervised!({BGP.Server, BGP.TestServerA}),
+      server_b: start_supervised!({BGP.Server, BGP.TestServerB})
     }
   end
 end
