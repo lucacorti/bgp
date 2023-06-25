fe08616bb549ec21843803c3df143b02701e204e
Luca Corti <luca@fantacast.it>
2023-06-25 20:11:45 +0200

    Move to elixir 1.15 and OTP 26

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BGP.MixProject do
     [
       app: :bgp,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
