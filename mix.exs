01e2dac7bc8993fc28803ba2e86bbe77e9f07972
Luca Corti <luca@fantacast.it>
2024-11-12 23:22:20 +0100

    Remove OTP < 24 and Elixir < 1.14 from CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BGP.MixProject do
     [
       app: :bgp,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
